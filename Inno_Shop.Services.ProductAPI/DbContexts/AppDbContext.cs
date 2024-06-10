1bd5675938ed72b922a59fe675c3aa74f3e26e3f
Maksim Berdnikau <max.berd.1900@gmail.com>
2024-06-11 00:05:44 +0300

    Добавьте файлы проекта.

---
+++ Inno_Shop.Services.ProductAPI/DbContexts/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inno_Shop.Services.ProductAPI.DbContexts;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+}
